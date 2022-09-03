1a9da4233697a64e3e652ae9ee24e3ba84740d02
unknown <paola.giusiano@correo.ucu.edu.uy>
2022-09-03 18:36:19 -0300

    Cambios en ejercicio 1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace SRP
 {
@@ -8,8 +11,8 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+       // public string LibrarySector { get ; set; }
+        //public string LibraryShelve { get ; set; }
 
         public Book(String title, String author, String code)
         {
@@ -18,11 +21,93 @@ namespace SRP
             this.Code = code;
         }
 
-        public void ShelveBook(String sector, String shelve)
+        
+        public override string ToString(){
+            return $"[{String.Join(',',(this.Title, this.Author, this.Code))}]";
+        } 
+
+        /*public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }*/
+
+    }
+
+
+    public class ShelveBook{
+
+        //private List<Book> libros = new List<Book>();
+        public static Dictionary<Book, (string, string)> dicc = new Dictionary<Book, (string,string)>(){};
+
+        public string LibrarySector { get ; set; } //sector
+        public string LibraryShelve { get ; set; } // shelve 7
+
+        
+
+        public ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
 
+        //método que agrega un libro a la estanteria 
+        public void agregarLibroShelveBook(Book libro/*, string sector, string shelve*/){
+            List<Book> keys = dicc.Keys.ToList();
+            List<(string,string)> values = dicc.Values.ToList();
+
+            (string, string) t1 = (this.LibrarySector, this.LibraryShelve);
+            if (values.Contains(t1)){
+                Console.WriteLine($"Ya esta ocupado el espacio {t1}");
+            }
+            else{
+                dicc.Add(libro, t1);
+            }
+        }
+
+        public string buscarLibro(Book libro){
+            List<Book> keys = dicc.Keys.ToList();
+            List<(string,string)> values = dicc.Values.ToList();
+            int i = 0;
+            bool salir = false;
+            while ( (i < keys.Count) && (!salir) ){
+                if (libro != keys[i]){
+                    i++;
+                }
+                else{
+                    salir = true;
+                }
+            }
+            string salida = "";
+            if (i < keys.Count){
+                salida = $"El libro {libro.Title} se encuentra en {values[i]}";
+            }
+            else{
+                salida = $"El libro {libro.Title} no se encuentra.";
+            }
+
+            return salida;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Sector: {this.LibrarySector} Shelve: {this.LibraryShelve}";
+        }
+
+        public void imprimirDicc(){
+            
+            Console.WriteLine("ELEMENTOS DEL DICCIONARIO :");
+            /*foreach(KeyValuePair<Book, (string,string)> item in dicc){
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }*/
+            foreach (var kvp in dicc) {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+          
+        }
     }
 }
+/*Para que la clase "Libro" cumpla con el principio SRP se debe hacer otra clase llamada "ShelveBook". 
+En esta útima se va a encontrar todos los métodos y atributos relacionados con la esatnteria. */
+
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,9 +9,29 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            //book1.ShelveBook("A","7");
+            //book2.ShelveBook("B","3");
 
+            ShelveBook shelve1 = new ShelveBook("A", "7");
+            ShelveBook shelve2 = new ShelveBook("B", "3");
+            ShelveBook shelve3 = new ShelveBook("B", "3");
+            
+            Console.WriteLine();
+
+            //Probando el método agregarLibroShelveBook e imprimirDicc
+            shelve1.agregarLibroShelveBook(book1);
+            shelve2.agregarLibroShelveBook(book2);
+            shelve3.agregarLibroShelveBook(book2); //espacio ocupado, por lo que no se puede agregar el libro
+            shelve1.imprimirDicc();
+
+            Console.WriteLine();
+
+            //Probando el método buscarLibro
+            Console.WriteLine(shelve1.buscarLibro(book2));
+            Book book3 = new Book("Hola Mundo","Anonimo","001-034");
+            Console.WriteLine(shelve1.buscarLibro(book3)); //libro  no agregado, por lo que no se encuentra
+
+             Console.WriteLine();
         }
     }
 }
